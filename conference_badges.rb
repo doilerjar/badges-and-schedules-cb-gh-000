e5821d5d8cff77317e3d5ae872d04c0f72e31ac5
Kevin K <doilerjar@outlook.com>
2018-08-07 02:49:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees)
+  badge_msgs = []
+  
+  attendees.each do |name|
+    badge_msgs << badge_maker(name)  
+  end 
+  return badge_msgs
+end 
